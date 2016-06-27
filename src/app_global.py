f0947c4d607bd52f11175e3b2eb9f2102cfc991b
Tianshi Cheng <chengts95@163.com>
2016-06-27 20:32:52 +0800

    remove database stuff

--- src/app_global.py
+++ src/app_global.py
@@ -1,21 +1,8 @@
 from database import *
-from datadef  import *
+
 
 clients_machine_ip = []
 clients_monitor_count = 0
 
 
 
-def get_key(req):
-    
-    pass
-
-def set_key(req):
-    pass
-
-def update_key(req):
-    pass    
-
-CLIENT_REQ_TYPES={GET_REQ:get_key,
-                  UPDATE_REQ:update_key,
-                  SET_REQ:set_key}
